182eebcf6fe573c09ae2abcc08f97664fd0c3a54
David Hochbaum <dhochbaum@planning.nyc.gov>
2025-11-14 11:09:13 -0500

    Fix for top 3 issues paragraphs

--- src/app/templates/top_3_issues.js
+++ src/app/templates/top_3_issues.js
@@ -1,4 +1,7 @@
+import fixString from "../helpers/fix_string"
+
 export default function Top3Issues(props) {
+  var top3IssuesRowCounter = 0;
 
   return (
 
@@ -8,17 +11,31 @@ export default function Top3Issues(props) {
       <p></p><p>The three most pressing issues facing this Community Board are:</p>
       <p></p>
       <p>
-        <b>{ props.cb.top3Issues1 !== "" ? props.cb.top3Issues1 : props.cb.top3Issues1Other}</b><br />
-        { props.cb.top3Issues1Explanation }
+        <b>{props.cb.top3Issues1 !== "" ? props.cb.top3Issues1 : props.cb.top3Issues1Other}</b>
       </p>
+      { fixString(props.cb.top3Issues1Explanation).map((row) => (
+        <p key={`top3issues1${top3IssuesRowCounter++}`}>
+          {row}
+        </p>
+      ))}
+
       <p>
-        <b>{ props.cb.top3Issues2 !== "" ? props.cb.top3Issues2 : props.cb.top3Issues2Other}</b><br />
-        { props.cb.top3Issues2Explanation }
+        <b>{props.cb.top3Issues2 !== "" ? props.cb.top3Issues2 : props.cb.top3Issues2Other}</b>
       </p>
+      { fixString(props.cb.top3Issues2Explanation).map((row) => (
+        <p key={`top3issues2${top3IssuesRowCounter++}`}>
+          {row}
+        </p>
+      ))}
+
       <p>
-        <b>{ props.cb.top3Issues3 !== "" ? props.cb.top3Issues3 : props.cb.top3Issues3Other}</b><br />
-        { props.cb.top3Issues3Explanation }
+        <b>{props.cb.top3Issues3 !== "" ? props.cb.top3Issues3 : props.cb.top3Issues3Other}</b>
       </p>
+      { fixString(props.cb.top3Issues3Explanation).map((row) => (
+        <p key={`top3issues3${top3IssuesRowCounter++}`}>
+          {row}
+        </p>
+      ))}
       <hr />
     </div>
 
